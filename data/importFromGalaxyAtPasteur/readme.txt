30c4511c6afff08da9fc2e8fd328d01848e4e63b
Olivia Doppelt-Azeroual <olivia.doppelt@pasteur.fr>
2014-10-23 00:31:04 +0200

    Addition of the extraction script to the repository and update of the readme file regarding the last commit

--- data/importFromGalaxyAtPasteur/readme.txt
+++ data/importFromGalaxyAtPasteur/readme.txt
@@ -8,40 +8,15 @@ to extract tool data from a Galaxy instance:
    2/ tool metadata extraction
 
 
-What to do: 
-   1/ Extract all available information for biotool xsd 
-in JSON format.
-   	Example for one tool: 
+What is included in this directory: 
+   1/ Extraction of  all available metadata from tools installed on a galaxy 
+   instance (institut pasteur's) for biotool xsd in JSON format.
+   	- Examples are now available in the github directory biotools_from_galaxy_at_pasteur
+	- the script galaxy_biotool_parser.py enables to parse a galaxy instance and get tools 
+	  installed from toolshed (it's the first version, it will be improved)
+	  --> requirments: bioblend api available on github (https://github.com/afgane/bioblend.git)
 
-
-	----------------------------------------
- {u'collectionName': 'GalaxyAtPasteur',
-  u'description': u'Soap aligner/soap2',
-  u'fonction': {u'description': u'Soap aligner/soap2',
-                u'functionHandle': 'MainFunction',
-                u'inputs': [{u'format': u'fastq, fasta',
-                             u'label': u'Read 1 preprocessed',
-                             u'name': u'fqinput',
-                             u'type': u'data'}],
-                u'outputs': [{u'format': u'text',
-                              u'label': u'Soap mapped reads',
-                              u'name': u'output'},
-                             {u'format': u'fasta',
-                              u'label': u'Soap UnMapped reads',
-                              u'name': u'unmappedreads'},
-                             {u'format': u'text',
-                              u'label': u'Soap UnPaired reads',
-                              u'name': u'unpairedreads'}]},
-  u'id': u'galaxy.web.pasteur.fr/toolshed-pasteur/repos/odoppelt/soap/soapAligner/2.21.1',
-  u'maturity': 'production',
-  u'name': u'Soap aligner',
-  u'platform': 'Linux',
-  u'softwareType': 'Tool',
-  u'sourceRegistry': u'toolshed-pasteur',
-  u'version': u'2.21.1'},
-  	 ----------------------------------------
-
-
+What's next: 
 
    2/ Json enrichment with EDAM ontologies and other 
 extraction data such as "ImportFromGensoftAtPasteur".
---
+++ data/importFromGalaxyAtPasteur/scripts/galaxy_biotool_parser.py
@@ -0,0 +1,110 @@
+'''
+Created on Oct. 23, 2014
+
+@author: Olivia Doppelt-Azeroual
+@contact: olivia.doppelt@pasteur.fr
+@project: toolinfowarehouse
+@organization: edamontology
+'''
+
+import sys
+import os
+import pprint
+import string
+import argparse
+
+from bioblend.galaxy.client import ConnectionError
+from bioblend.galaxy import GalaxyInstance
+
+
+def build_tool_name(tool_id):
+    """
+    @tool_id: tool_id
+    builds the tool_name regarding its toolshed id
+   """
+    print tool_id
+    id_list = string.split(tool_id, '/')
+    return string.join(id_list[-2:], '_')
+
+
+def get_source_registry(tool_id):
+    try:
+        tool_id.index('toolshed')
+        source = string.split(tool_id, '/')
+        for i in range(len(source) - 1):
+            if source[i].find('toolshed'):
+                return source[i + 1]
+    except ValueError:
+        print 'ValueError:', tool_id
+        return ''
+
+
+def build_metadata_one(tool_meta_data):
+    """
+      builds general_dict
+      @param: tool_meta_data for one tool extracted from galaxy
+    """
+    gen_dict = {k: tool_meta_data[k] for k in (u'name', u'id', u'version', u'description')}
+    gen_dict[u'collectionName'] = 'GalaxyAtPasteur'
+    gen_dict[u'softwareType'] = 'Tool'
+    gen_dict[u'sourceRegistry'] = get_source_registry(gen_dict[u'id'])
+    gen_dict[u'maturity'] = 'production'
+    gen_dict[u'platform'] = 'Linux'
+    return gen_dict
+
+
+def build_fonction_dict(tool_meta_data):
+    """
+    builds function dict
+    2 steps for inputs, get only the data format and
+    dict comprehension to keep only important info
+    1 steps for outputs, only dict comprehension
+    """
+    func_dict = {}
+    inputs = []
+    outputs = []
+    inputs_complete = [elem for elem in tool_meta_data[u'inputs'] if elem[u'type'] in [u'data']]
+    for input in inputs_complete:
+        # ici mettre un try catch pour gerer 'extensions' et non 'format'"
+        elem = {i: input[i] for i in (u'format', u'type', u'label', u'name')}
+        inputs.append(elem)
+    for output in tool_meta_data[u'outputs']:
+        outputs.append({o: output[o] for o in (u'format', u'label', u'name')})
+
+    func_dict[u'description'] = tool_meta_data[u'description']
+    func_dict[u'outputs'] = outputs
+    func_dict[u'inputs'] = inputs
+    func_dict[u'functionHandle'] = 'MainFunction'
+    return func_dict
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description = "Galaxy instance tool parsing, for integration in biotools/bioregistry")
+
+    parser.add_argument("--galaxy_url", help="url to the analyzed galaxy instance")
+    parser.add_argument("--api_key", help="galaxy user api key")
+    parser.add_argument("--tool_dir", help="directory to store the tool json (needs to be created before running the script")
+
+    args = parser.parse_args()
+    gi = GalaxyInstance(args.galaxy_url, key=args.api_key)
+
+    tools = gi.tools.get_tools()
+    tools_meta_data = []
+    new_dict = {}
+    json_ext = '.json'
+
+    for i in tools:
+        try:
+            if not i['id'].find("galaxy.web.pasteur.fr") or not i['id'].find("testtoolshed.g2.bx.psu.edu") or  not i['id'].find("toolshed.g2.bx.psu.edu"):
+                tools_meta_data.append(gi.tools.show_tool(tool_id=i['id'], io_details=True, link_details=True))
+        except ConnectionError:
+            pass
+
+    for tool in tools_meta_data:
+        tool_name = build_tool_name(tool[u'id'])
+        tool_file = open(os.path.join(os.getcwd(), args.tool_dir, tool_name + json_ext), 'w')
+        general_dict = build_metadata_one(tool)
+        general_dict[u'fonction'] = build_fonction_dict(tool)
+        pprint.pprint(general_dict, tool_file)
+        tool_file.close()
