44e59f16307dc7b95e3da3ff0c0f2f18b6fc7699
Olivia Doppelt-Azeroual <olivia.doppelt@pasteur.fr>
2015-01-22 16:18:48 +0100

    inputs case are put in separate json files next step EDAM ontologies instead of format name

--- data/importFromGalaxyAtPasteur/scripts/galaxy_biotool_parser.py
+++ data/importFromGalaxyAtPasteur/scripts/galaxy_biotool_parser.py
@@ -9,6 +9,7 @@ Created on Oct. 23, 2014
 
 import sys
 import os
+import re
 import pprint
 import string
 import argparse
@@ -117,7 +118,7 @@ def build_case_inputs(case_dict, input):
                                         dict_cases[inp[u'value']].append(case_input)
 
                 except KeyError:
-                    print "KeyError key == REPEAT"
+#                    print "KeyError key == REPEAT"
                     for el in elem[u'inputs']:
                         if el[u'type'] == u'data':
                             if dict_cases.get(inp[u'value']) is None:
@@ -176,6 +177,7 @@ def build_fonction_dict(tool_meta_data):
     1 steps for outputs, only dict comprehension
     """
     func_dict = {}
+    func_list = []
     inputs = {}
     outputs = []
     inputs_fix = []
@@ -187,9 +189,7 @@ def build_fonction_dict(tool_meta_data):
             inputs_fix.append(input)
         # repeat not in conditional
         if input[u'type'] == u'repeat':
-
             for rep in input[u'inputs']:
-
                 if rep[u'type'] == u'data':
                     print 'repeeeeeeaaaaaatttt'
                     inputs_fix.append(rep)
@@ -198,10 +198,6 @@ def build_fonction_dict(tool_meta_data):
         if input[u'type'] == "conditional":
             build_case_inputs(dict_cases, input)
 
-    print "______cases_dict: "
-    pprint.pprint(dict_cases)
-    print "______inputs_complete"
-    pprint.pprint(inputs_fix)
 
 #__________________INPUT DICT _________________________
     if len(dict_cases) == 0:
@@ -209,9 +205,6 @@ def build_fonction_dict(tool_meta_data):
     else:
         for key, case in dict_cases.iteritems():
             inputs[key] = build_input_for_json(case) + build_input_for_json(inputs_fix)
-
-
-# Faire 2 inputDict si 2 cases --> 2  tools a enregistrer
 
 
 #_____________OUTPUT DICT_______________________________________
@@ -223,13 +216,26 @@ def build_fonction_dict(tool_meta_data):
         outputDict[u'dataHandle'] = output[u'name']
         outputs.append(outputDict)
 
-    func_dict[u'description'] = tool_meta_data[u'description']
-    func_dict[u'functionName'] = []
-    func_dict[u'output'] = outputs
-    func_dict[u'input'] = inputs
-    func_dict[u'functionHandle'] = 'MainFunction'
+    if inputs.get("input_fix") is None:
+        for input_case_name, item in inputs.items():
+            func_dict = {}
+            func_dict[u'description'] = tool_meta_data[u'description']
+            func_dict[u'functionName'] = []
+            func_dict[u'output'] = outputs
+            func_dict[u'input'] = item
+            func_dict[u'functionHandle'] = 'MainFunction'
+            func_dict[u'annot'] = input_case_name
+            func_list.append(func_dict)
+    else:
+        func_dict[u'description'] = tool_meta_data[u'description']
+        func_dict[u'functionName'] = []
+        func_dict[u'output'] = outputs
+        func_dict[u'input'] = inputs[u"input_fix"]
+        func_dict[u'functionHandle'] = 'MainFunction'
+        func_list.append(func_dict)
+#        print("TYPE FUNC DICT:", type(func_dict))
 
-    return func_dict
+    return func_list
 
 
 if __name__ == "__main__":
@@ -251,7 +257,7 @@ if __name__ == "__main__":
     new_dict = {}
     json_ext = '.json'
 
-    for i in tools[0:5]:
+    for i in tools:
         try:
             # improve this part, important to be able to get all tool from any toolshed
             if not i['id'].find("galaxy.web.pasteur.fr") or not i['id'].find("testtoolshed.g2.bx.psu.edu") or not i['id'].find("toolshed.g2.bx.psu.edu"):
@@ -261,15 +267,34 @@ if __name__ == "__main__":
           #  else:
            #     print i['id']
         except ConnectionError:
+            print "ConnectionError"
             pass
 
     for tool in tools_meta_data:
         tool_name = build_tool_name(tool[u'id'])
-        with open(os.path.join(os.getcwd(), args.tool_dir, tool_name + json_ext), 'w') as tool_file:
-            try:
-                general_dict = build_metadata_one(tool, args.galaxy_url)
-                general_dict[u'fonction'] = build_fonction_dict(tool)
-                json.dump(general_dict, tool_file, indent=4)
-                tool_file.close()
-            except SystemExit:
-                pass
+        try:
+
+            function = build_fonction_dict(tool)
+            #print "TYPE FUNCTION:", type(function)
+            if len(function) > 1:
+                print "THERE WILL BE  " + str(len(function)) + "json"
+                for func in function:
+                    pprint.pprint(func)
+                    #inputs = func[u"input"]
+                    name = re.sub("[\.\,\:;\(\)\./]", "_", func[u'annot'], 0, 0)
+                    with open(os.path.join(os.getcwd(), args.tool_dir, tool_name + "_"+ name + json_ext), 'w') as tool_file:
+                        general_dict = build_metadata_one(tool, args.galaxy_url)
+                        general_dict["function"] = func
+                        json.dump(general_dict, tool_file, indent=4)
+                        tool_file.close()
+
+            else:
+                with open(os.path.join(os.getcwd(), args.tool_dir, tool_name + json_ext), 'w') as tool_file:
+                    general_dict = build_metadata_one(tool, args.galaxy_url)
+                    general_dict["function"] = function[0]
+                    json.dump(general_dict, tool_file, indent=4)
+                    tool_file.close()
+
+
+        except SystemExit:
+            pass
