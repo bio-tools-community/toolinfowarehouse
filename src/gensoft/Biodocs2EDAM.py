6afbacb15d63f918d31a297c519a400b6c68f998
Eric  DEVEAUD <edeveaud@pasteur.fr>
2015-02-03 10:08:50 +0100

    Biodocs conversion rewrite

---
+++ src/gensoft/Biodocs2EDAM.py
@@ -0,0 +1,291 @@
+#! /usr/bin/env python 
+
+import argparse
+import json
+import os
+import sys
+
+from BiodocParser import *
+
+# tweak modulecmd in order to use 3.3.a updated (not yet released)
+os.environ['MODULESHOME'] = ''
+os.environ['PY_MODULECMD'] = '/local/gensoft2/adm/Modules/3.3.a/bin/modulecmd'
+import module as m
+
+import module as m
+
+INDENT=4
+
+EDAM_URI = "http://edamontology.org"
+GENSOFT_MAINTAINER='gensoft@pasteur.fr'
+OLIVIAS_UNIQUE_SEPARATOR = '#@#'
+
+DEFAULT_TOPIC=3
+DEFAULT_OPERATION=4
+
+FATAL= 1
+WARN= 0
+
+def error(exit_val, *msg):
+    head=['Warning', 'Error']
+    print >> sys.stderr, "%s: %s" % (head[exit_val], " - ".join(map(str, msg)))
+    if exit_val:
+        sys.exit(exit_val)
+    return None
+
+
+def ontology_parser(fh):
+    onto = {}
+    for line in fh:
+        line=line.strip()
+        edam_id, desc, edam_uri = line.split(OLIVIAS_UNIQUE_SEPARATOR)
+        kind, edam_number = edam_id.split(':')
+        k = int(edam_number)
+        onto[k] = { 'id': edam_id
+                  , 'desc' : desc
+                  , 'kind' : kind
+                  , 'uri' : edam_uri
+                  }
+    return onto
+
+
+def gensoft_categories2EDAM(term,ontology):
+    # gensoft format: EDAM-0002241 --> convert to int
+    _, key = term.split('-') 
+    key = int(key)
+    if key not in ontology: 
+        return None
+    return  ontology[key]
+ 
+def get_biodocs_contact(biodoc_descriptor):
+    return [ { "contactEmail": GENSOFT_MAINTAINER
+             , "contactURL": ''
+             , "contactName": ''
+             , "contactTel": ''
+             , "contactRole": "Technical"
+             }
+            ]
+
+def get_biodocs_interface(biodoc_descriptor):
+ return [
+         { "interfaceType": { "term": "Command line",
+           "uri": "http://www.cbs.dtu.dk/ontology/interface_type/5" }
+         }
+        ]
+
+def get_biodocs_name(biodoc_descriptor):
+    return biodoc_descriptor['NAME']
+
+
+def get_biodocs_homepage(biodoc_descriptor):
+    return biodoc_descriptor['HOME'] if biodoc_descriptor['HOME'] else None
+
+
+def get_biodocs_description(biodoc_descriptor):
+    return ' '.join(biodoc_descriptor['DESCRIPTION'])
+
+
+def get_biodocs_authors(biodoc_descriptor):
+    # as authors are not documented with this granularity in 
+    # biodocs, we set up all the authors to contributors.
+    return {"creditsContributor": biodoc_descriptor['AUTHORS']}
+
+
+def get_module_docs(package, version):
+    # return biodoc_descriptor['HTMLDOCS'] + biodoc_descriptor['MANPAGES']
+    def get_docs(info):
+        '''
+        get local documentation available for given package
+        returns a tuple (htmldocs, txtdocs) of list as str
+        '''
+        htmldocs = []
+        txtdocs = []
+        keep = False
+        for elem in infos:
+            elem = elem.strip()
+            if not elem:
+                continue
+            if elem == "local documentation available:":
+                keep = True
+                continue
+            elif elem == "package provides following commands:":
+                break
+            if keep:
+                if 'bioweb2' in elem:
+                    htmldocs.append(elem)
+                else:
+                    txtdocs.append(elem)
+        return htmldocs, txtdocs 
+
+    module= "%s/%s" % (package, version)
+    try:
+        infos=m.modulehelp(package).split('\n')
+    except RuntimeError as err:
+        error(FATAL, "module Error", err)
+
+    return get_docs(infos)[0]    
+    
+
+
+def get_biodocs_topics(biodoc_descriptor, ontology_desc):
+    topics = []
+    for cat in biodoc_descriptor['CATEGORIES']:
+        edam=gensoft_categories2EDAM(cat, ontology_desc)
+        if edam is None:
+            error(WARN, cat, 'unknonw category')
+            continue
+        if edam['kind'] != 'EDAM_topic':
+            continue
+        res={ 'term': edam['id']
+            , 'uri' : edam['uri']
+            }
+        topics.append(res)
+    if not topics:
+        default = ontology_desc[DEFAULT_TOPIC]
+        res={ 'term': default['id']
+            , 'uri' : default['uri']
+            }
+        topics.append(res)
+    return topics
+
+def get_biodocs_functions(biodoc_descriptor, ontology_desc):
+    functions = []
+    for cat in biodoc_descriptor['CATEGORIES']:
+        edam=gensoft_categories2EDAM(cat, ontology_desc)
+        if edam is None:
+            error(WARN, cat, 'unknonw category')
+            continue
+        if edam['kind'] != 'EDAM_operation':
+            continue
+        res={ 'functionName': [ {'term': edam['id']
+                                , 'uri' : edam['uri']
+                                }
+                              ]
+            }
+        functions.append(res)
+    if not functions:
+        default = ontology_desc[DEFAULT_OPERATION]
+        res={ 'term': default['id']
+            , 'uri' : default['uri']
+            }
+        functions.append(res)
+    return functions
+    
+def get_biodocs_publications(biodoc_descriptor):
+    publications=[publi for publi in biodoc_descriptor['REF'] if publi ]
+    refs = []
+    for publi in publications:
+        _, pub_id = get_doi(publi)
+        if pub_id is None: continue
+        fields = pub_id.split(':')
+        qualifier = fields[0]
+        if qualifier.lower() != 'doi':
+            continue
+        pub_id = ":".join(fields[1:])
+        refs.append(pub_id.strip())
+    publis = {}
+    if refs:
+        publis = {'publicationsPrimaryID': refs[0] if refs else []
+                 ,'publicationsOtherID': refs[1:]
+                 }
+    return publis
+
+
+def get_biodoc_ressource(biodoc_descriptor):
+    return [ { "term": "Suite" } ]
+
+def get_biodocs_versions(biodocs):
+    return [elem.replace('(default)', '') for elem in biodocs['VERSION']]
+
+def to_document(biodocs):
+    if biodocs['PRIVATE']:
+        return False
+    if biodocs['LIBRARY']:
+        return False
+    if not biodocs['HOME']:
+        return False
+    return True
+
+def jason_generator(biodocs, outdir):
+
+    pack_name = get_biodocs_name(biodocs)
+
+    #---- check if worth the documentation
+    if not to_document(biodocs):
+        error(WARN, pack_name, 'not documented')
+        return
+
+    res = {}
+    #---- tag with gensoft origin
+    res['affiliation'] = "Gensoft @Institut Pasteur"
+
+    #---- common information to all packages version available in 
+    #---- gensoft BIODOCS
+    #---- mandatory info 
+    res['name'] = pack_name
+    res['homepage'] = get_biodocs_homepage(biodocs)
+    res['resourceType'] = get_biodoc_ressource(biodocs)
+    res['interface'] = get_biodocs_interface(biodocs)
+    res['description'] = get_biodocs_description(biodocs)
+    res['topic'] = get_biodocs_topics(biodocs, onto)
+    res['function'] = get_biodocs_functions(biodocs, onto)
+    res['contact'] = get_biodocs_contact(biodocs)
+    
+    #---- accessory info
+    publication = get_biodocs_publications(biodocs)
+    if publication:
+        res['publications'] = publication
+    credits = get_biodocs_authors(biodocs)
+    if credits:
+        res['credits'] = credits
+
+    versions = get_biodocs_versions(biodocs)
+    for version in versions:
+        res['version'] = version
+        # bidocs docs description is broken, no versionnig.
+        # so superseed it with the one grabbed from module
+        res['docs'] = get_module_docs(pack_name, version)
+        
+        outfile = "%s_%s.json" %(pack_name, version)
+        outfile = os.path.join(outdir, outfile)
+        print "dump to %s" %(outfile) 
+        try:
+            outfh= open(outfile, 'w')
+        except IOError as msg:
+            error(FATAL, outfile, msg)
+        
+        json.dump(res, outfh, indent=INDENT)
+        outfh.close()
+        
+
+
+
+if __name__ == '__main__':
+
+
+    #---- get command line
+    parser = argparse.ArgumentParser(description='Biodocs to EDAM registry conversion tool.')
+    parser.add_argument('-e', '-edam_ontology', action="store"
+                        , type=str
+                        , default=None
+                        , dest='ONTO'
+                        , help='EDAM ontology description file .')
+    parser.add_argument('-o', '-out-dir', action="store"
+                        , type=str
+                        , default='json'
+                        , dest='OUTDIR'
+                        , help='json outdir.')
+    parser.add_argument('args', nargs='*')
+    cmdline = parser.parse_args()
+
+    ontology_file = cmdline.ONTO
+    outdir = cmdline.OUTDIR
+
+    if ontology_file is None:
+        error(FATAL, "missing ontology description: abort")
+    with open(ontology_file, 'r') as fh:
+        onto = ontology_parser(fh)
+    for biodoc_file in cmdline.args:
+        with open(biodoc_file, 'r') as fh:
+            biodocs = Parser(fh)[0]
+            jason_generator(biodocs, outdir)
---
+++ src/gensoft/module.py
@@ -0,0 +1,339 @@
+"""
+A python wrapper to the Module Environment command.
+see http://modules.sourceforge.net/
+
+Warning: requires Module Environment  >= 3.3.*
+pre 3.3 version may silently fail on some commands dur to exit values
+of modulecmd non consistent. 
+"""
+
+#  initial author: Robert Minsk (egbert@centropolisfx.com)
+# Feb   1, 2001          Robert Minsk (egbert@centropolisfx.com)
+#      * init file for python added
+# Oct  1, 2010          R.K. Owen (rk@owen.sj.ca.us)
+#     * sed dependency removed
+#     * name changing  
+# Sep 15,1014         Eric Deveaud (edeveaud@pastuer.fr)
+#     * some code rewrite
+#     * added python bindings to common calls to module command
+#     * proposed setup.py installation mechanism
+
+import collections
+import os
+import re
+import subprocess
+import sys
+
+#--- check for module installation  
+if 'MODULESHOME' not in os.environ:
+    raise OSError("missing MODULESHOME definition or non available Module Environment")
+   
+#--- set necesssary env var if not already positioned
+if  'MODULEPATH' not in os.environ:
+    initfile = os.path.join(os.environ['MODULESHOME'], 'init', '.modulespath')
+    with open(initfile, "r") as infh:
+        modulepath = []
+        for line in infh:
+            #---- comment character `#' may be in any position
+            line = line.split('#')[0].strip()
+            if  line:
+                modulepath.append(line)
+    os.environ['MODULEPATH'] = ":".join(modulepath)
+
+if 'LOADEDMODULES' not in os.environ:
+    os.environ['LOADEDMODULES'] = ''
+
+#---- get full path to modulecmd if defined in environment else fallback
+#  to one availble in path
+# If PY_MODULECMD is defined, Modulecmd will always  use its value
+# to invoke modulecmd. Otherwise, it will attempt to invoke "modulecmd",
+# relying on it being in the PATH.
+_modulecmd_ = os.environ.get('PY_MODULECMD', "modulecmd")
+#---- check for command availability
+with open(os.devnull, 'w') as _DEVNULL:
+    try:
+        subprocess.check_call([_modulecmd_, "-V"], stdout=_DEVNULL, stderr=_DEVNULL)
+    except OSError as err:
+        raise OSError("missing MODULESHOME definition or non available Module Environment")
+
+
+
+#---- for python 2 & 3 compatibility
+def _b(x):
+    if sys.version < '3':
+        return x
+    else:
+        return x.decode()
+
+def module(*args):
+    """
+    wrapper to the module command.
+    accept the same switches, subcommand and subcommands-args  
+    than the shell command
+    for subcommand with display information returns a tuple composed by
+    stdout and stderr (where results are) execution of modulecmd.
+    otherwise None is returned and the environment is modified regarding 
+    the executed module subcommand.
+    """
+    if isinstance(args[0], list):
+        args = args[0]
+    elif isinstance(args[0], str):
+        args = args[0].split()
+    else:
+        args = list(args)
+    # -=-=-=-=-=-=-=-=-=-=-  WARNING  -=-=-=-=-=-=-=-=-=-=-
+    interpreter = 'python'
+    try:
+        module_process = subprocess.Popen(
+            [_modulecmd_, interpreter] + args,
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE,
+						close_fds=True)
+    except OSError as msg:
+        raise OSError(msg)
+    (output, error) = module_process.communicate()
+    #deal with different type returned by subprocess beetwen python-2 and python-3
+    output = _b(output)
+    error = _b(error)
+    if module_process.returncode != 0:
+        msg = "module command failure"
+        raise RuntimeError(msg, output, error)
+
+    #---- check for submited module subcommand
+    #---- for those that display info on stderr,  return results to caller
+    verbose_command = (
+        'display', 'show', 'avail', 'av', 'list', 
+        'help', 'whatis', 'apropos', 'keyword',
+        '-V', '--version', '-H', '--help',
+        )
+    for subcommand in verbose_command:
+        if subcommand in args:
+            return output, error
+    #--- non verbose command do the job
+    exec(output)
+
+
+
+def _subcommand_runner(subcommand, *args):
+    '''
+    internal subcommand dipsatcher
+    '''
+
+    if not args:
+        args = []
+    elif isinstance(args[0], list):
+        args = args[0]
+    elif isinstance(args[0], tuple):
+        args = list(args[0])
+    else:
+        args = list(args)
+
+    ret = None
+    #---- module managment subcommands
+    if subcommand in ('add', 'load', 'rm', 'unload', 'switch', 'swap'):
+        module([subcommand] + args)
+    elif subcommand in ('purge', 'refresh'):
+        module(subcommand)
+    elif subcommand in ('list', 'av', 'avail', 'available'):
+        # force -terse switch in order to facilitate results parsing  
+        _, e = module([subcommand] + ['-t'] + args)
+        if subcommand == 'list':
+            ret = [item for item in e.split('\n') if item and 'Currently Loaded' not in item]
+        else:
+            ret = [item for item in e.split('\n') if item and not  item.startswith('/')]
+    #---- modulepath managment subcommands
+    elif subcommand in ('use', 'unuse'):
+        module([subcommand]  + args)
+    #---- module informative  subcommand 
+    elif subcommand in ('display', 'show', 'help', 'whatis', 'apropos', 'keyword'):
+        _, ret = module([subcommand] + args)
+        #ret = ret.split('/n')
+    else:
+        raise ValueError("unknow subcommand {0}".format(subcommand))
+    return ret
+         
+
+def moduleload(*args):
+    '''
+    Load modulefile(s) into the environment
+    '''
+    return _subcommand_runner('load', *args)
+
+
+def moduleunload(*args):
+    '''
+    Remove modulefile(s) from the environment.
+    '''
+    return _subcommand_runner('unload', *args)
+
+
+def moduleswap(modulefile1, *modulefile2):
+    '''
+    Switch loaded  modulefile1  with  modulefile2.
+    If   modulefile2  is  not specified,  then  it is assumed to be the
+    currently loaded module with the  same root name as
+    modulefile1.
+    '''
+    return _subcommand_runner('swap', modulefile1, *modulefile2)
+
+
+def moduledisplay(*args):
+    '''
+    Return as a string the  information    about   one   or   more
+    modulefiles.  moduledisplay  will  list
+    the  full  path  of the modulefile(s) and all (or
+    most)   of   the    environment    changes    the
+    modulefile(s)  will make if loaded.
+    '''
+    return _subcommand_runner('display', *args)
+
+def moduleapropos(*args):
+	'''
+	Seeks through  the  'whatis'  informations  of  all  modulefiles
+	for  the specified string.
+	 All module-whatis informations matching the string will be displayed.
+	'''
+	return _subcommand_runner('apropos', *args)
+
+def moduleavail(*args, **icase):
+    '''
+    returns a list of modulefiles matching the given prefix.
+    icase: case insensitive search if True
+                otherwise case sensitive search
+    '''
+    cmd = 'av'
+    if icase:
+        return _subcommand_runner('av', '-i', *args)
+    return _subcommand_runner(cmd, *args)
+
+
+def moduleuse(*args, **append_path):
+    '''
+    Prepend  one  or  more directories to the MODULEPATH environment
+    variable.
+    when append_path=True is given on the arguments, directories will be
+    appended to MODULEPATH 
+    '''
+    #---- check  directories
+    #for d in args:
+    #    if not os.path.isdir(d):
+    #        raise IOError(d, 'no such file or directory') 
+    if append_path:
+        return _subcommand_runner('use', '-a',  *args)
+    return _subcommand_runner('use', *args)
+
+
+def moduleunuse(*args):    
+    '''
+    Remove  one  or   more   directories   from   the
+    MODULEPATH environment variable.
+    '''
+    return _subcommand_runner('unuse', *args)
+
+
+def modulerefresh():
+    '''
+    Force  a refresh of all non-persistent components
+    of currently loaded modules.
+    '''
+    return _subcommand_runner('refresh')
+
+
+def modulepurge():
+    '''
+    Unload all loaded modulefiles.
+    '''
+    return _subcommand_runner('purge')
+
+
+def modulelist():
+    '''
+    returns a list of currently loaded modulefiles.
+    '''
+    return _subcommand_runner('list')
+
+def modulehelp(*args):
+    '''
+    Print  the  usage of each sub-command.  If an argument is given, print the
+    Module-specific help information for the modulefile(s)
+    '''
+    return _subcommand_runner('help', *args)
+
+def getmoduleversion():
+    '''
+    returns modules version as string
+    '''
+    import re
+    output, error = module('--version')
+    refilter = re.compile('VERSION=(?P<version>(.*))\n')
+    hit =  refilter.search(error)
+    if not hit:
+        raise RuntimeError("unknow format: {0}".format(error))
+    return  hit.group('version')
+
+
+def modulewhatis(*args):
+    '''
+    retur as a string  the information set  up  by  the  module-
+    whatis     commands    inside    the    specified
+    modulefile(s). If no modulefile is specified, all
+    'whatis' lines will be shown.
+    '''
+    return _subcommand_runner('whatis', *args)
+
+#---- currently broken due to modulecmd exiting with failure return value 
+#---- need work to be done on the C code of modulecmd
+
+def _moduleapropos(*args):
+    '''
+    Seeks  through  the  'whatis' informations of all
+    modulefiles  for  the  specified   string.
+    '''
+    return _subcommand_runner('whatis', *args)
+
+
+"""
+#---- pasteur internal, will maybee find is way to final distribution.
+
+#---- structure describing the modules
+modulefile = collections.namedtuple("modulefile", "name default versions")
+
+def _av2tuple(lst):
+    m = {}
+    pattern = '.*/(?P<vdef>.*)\(default\)'
+    automat = re.compile(pattern) 
+    for item in lst:
+        #---- get name and version numbers
+        fields = item.split(os.sep)
+        name = fields[0]
+        version = []
+        if len(fields) > 1:
+            version = ["/".join([e.replace('(default)', '')  for e in   fields[1:]])]
+        #---- versions[0] will contain the default version is found else None
+        m[name] = m.get(name, [None]) + version 
+        #---- get default version if present
+        hit = automat.search(item)
+        if hit:
+            default = hit.group('vdef')
+            m[name][0] = default
+    #--- convert all to named tuple
+    availablemodules = []
+    for name in m:
+        vdef = m[name][0]
+        versions = m[name][1:]
+        availablemodules.append(modulefile(name, vdef, versions)) 
+    return availablemodules
+
+
+
+def module2soft(*args):
+    for m in args:
+        helpmod = modulehelp(m).split('\n')
+        i = 0
+        for item in helpmod:
+            i+=1
+            if item == 'package provides following commands:':
+                break
+        return [item[1:] for item in helpmod[i:] if item]
+        
+"""
